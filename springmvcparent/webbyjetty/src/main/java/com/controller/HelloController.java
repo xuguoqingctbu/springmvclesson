d11b0321f122e7bbbf2c934efca6c8774b0256d2
qingge <1761368665@qq.com>
2020-10-12 19:06:24 +0800

    Initial commit

---
+++ springmvcparent/webbyjetty/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+  public String hello(){
+        System.out.println("欢迎使用jetty springmvc");
+        return "hello";
+    }
+
+}
---
+++ springmvcparent/webbyjetty/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,10 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>欢迎使用springmvc! by jetty</h1>
+</body>
+</html>
